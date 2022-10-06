70b8e26c4c28399595dfe1a5bf75e8bbe6ed5acd
Aditya Baireddy <adbaireddy@berkeley.edu>
2022-10-06 16:45:19 -0700

    random problem solving

--- Project Euler/test.py
+++ Project Euler/test.py
@@ -3,20 +3,29 @@ import functions as f
 import timeit 
 import math
 import doctest
-from random import random 
-N = 10 ** 7
+from random import random, randint
+N = 10 ** 3
 count = 0
-fail_num = 0
-for _ in range(N):
-    count += 1
-    x = 0
-    y = random()
-    while y > x:
-        x = y
-        y = random()
-        count += 1
-    fail_num += x
-print(f"avg count: {count/N}")
-print(f"avg last num: {fail_num/N}") 
-    
+def max_greater_than(n):
+    for i in range(12):
+        if n < i*i*i:
+            return i-1
 
+for i in range(1, N+1):
+    x = max_greater_than(i)
+    if i % x == 0:
+        count += 1
+print(count * 5, N - count)
+
+successes = 0
+fails = 0
+trials = 10 ** 7
+for _ in range(trials):
+    a = randint(1, 1000)
+    b = max_greater_than(a)
+    if a % b:
+        fails += 1
+    else:
+        successes += 1
+
+print((5*successes - fails)/trials)
